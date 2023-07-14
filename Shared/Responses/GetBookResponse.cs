1e562038c0b42612696a47a0bbe57c814b87659f
April.Forones <aprilforones13@gmail.com>
2023-07-14 16:15:32 +0800

    Update071423

---
+++ Shared/Responses/GetBookResponse.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Responses
+{
+    public class GetBookResponse
+    {
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public GetAuthor Author { get; set; }
+    }
+
+    public class GetAuthor
+    {
+        public int ID { get; set; }
+        public string FName { get; set; }
+        public string LName { get; set; }
+    }
+}
