1181b938f3cf88234026e94e2dc244be84e3ede4
April.Forones <aprilforones13@gmail.com>
2023-07-17 13:47:39 +0800

    connect

--- MauiApp1/Pages/Index.razor.cs
+++ MauiApp1/Pages/Index.razor.cs
@@ -8,6 +8,31 @@ namespace MauiApp1.Pages
 
     {
         public List<Book> RetrievedBooks { get; set; } = new List<Book>();
+        private string networkstatus;
+        public Index()
+        {
+            Connectivity.ConnectivityChanged += OnConnectivityChanged;
+        }
+
+        private void OnConnectivityChanged(object sender, ConnectivityChangedEventArgs e)
+        {
+            if (e.NetworkAccess == NetworkAccess.Internet)
+
+            {
+
+                networkstatus = "Connected";
+
+            }
+            else
+            {
+
+                networkstatus = "Not Connected";
+
+            }
+
+            StateHasChanged();
+        }
+
         protected override async Task OnInitializedAsync()
         {
             var client = new HttpClient();
